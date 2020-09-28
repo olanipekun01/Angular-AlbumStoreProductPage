15ffc6785231dd62b40ab7141f034b831e710cd3
olanipekun01 <oloniluaeyitayo@gmail.com>
2020-09-28 22:47:35 +0100

    changed  my getAlbum method parameter datatype from Uppercase to Lowercase

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -5,7 +5,7 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class ProductService {
   private _albumUrl = "../assets/album.json";
-  getAlbum(id: Number) {
+  getAlbum(id: number) {
     return this._http.get(this._albumUrl).map(response => response.json());
   }
 
