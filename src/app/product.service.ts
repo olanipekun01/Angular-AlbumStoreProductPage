7ff48065184a1350baf74d6607dcf5cdb1db4a17
olanipekun01 <oloniluaeyitayo@gmail.com>
2020-09-29 21:09:00 +0100

    added product listingcomponents

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 import { Observable } from 'rxjs/Observable';
-import { AlbumStorePage } from '../../e2e/app.po';
 
 import { Album } from './album';
 
